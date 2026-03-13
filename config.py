e2b2d21ad6f79b92df262f83614fca6123112d1c
AleSava3 <alessiojuve03@gmail.com>
2026-03-13 18:18:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -6,9 +5,13 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 MAX_SIGNALS_PER_DAY = 20
 
-MIN_SCORE = 85
+# score minimo più flessibile
+MIN_SCORE = 80
+
+# segnali top
 INVEST_SCORE = 92
 
+# risk management crude oil
 ATR_SL_OIL = 1.5
 ATR_TP_OIL = 3.2
 
